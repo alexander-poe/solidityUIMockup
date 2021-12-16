99f64e514227f4241f99b443ae411e1749bb08f7
poe <poe@poes-MacBook-Air.local>
2021-12-16 15:33:07 -0800

    saving work, switching computers, great commit message.

--- script.js
+++ script.js
@@ -1,6 +1,449 @@
 log = console.log.bind(console, " Script.js : ")
 const owner = false
 
+const address = '0xd9145CCE52D386f254917e481eB44e9943F39138'
+
+var abi = [{
+	"compiler": {
+		"version": "0.8.7+commit.e28d00a7"
+	},
+	"language": "Solidity",
+	"output": {
+		"abi": [
+			{
+				"inputs": [],
+				"stateMutability": "nonpayable",
+				"type": "constructor"
+			},
+			{
+				"anonymous": false,
+				"inputs": [
+					{
+						"indexed": false,
+						"internalType": "uint256",
+						"name": "gtin",
+						"type": "uint256"
+					},
+					{
+						"indexed": false,
+						"internalType": "string",
+						"name": "productName",
+						"type": "string"
+					}
+				],
+				"name": "AuctionActive",
+				"type": "event"
+			},
+			{
+				"anonymous": false,
+				"inputs": [
+					{
+						"indexed": false,
+						"internalType": "uint256",
+						"name": "auctionNum",
+						"type": "uint256"
+					}
+				],
+				"name": "AuctionCancelled",
+				"type": "event"
+			},
+			{
+				"anonymous": false,
+				"inputs": [
+					{
+						"indexed": false,
+						"internalType": "uint256",
+						"name": "gtin",
+						"type": "uint256"
+					},
+					{
+						"indexed": false,
+						"internalType": "string",
+						"name": "productName",
+						"type": "string"
+					}
+				],
+				"name": "AuctionClosed",
+				"type": "event"
+			},
+			{
+				"anonymous": false,
+				"inputs": [
+					{
+						"indexed": false,
+						"internalType": "uint256",
+						"name": "gtin",
+						"type": "uint256"
+					},
+					{
+						"indexed": false,
+						"internalType": "string",
+						"name": "productName",
+						"type": "string"
+					},
+					{
+						"indexed": false,
+						"internalType": "uint256",
+						"name": "highestBid",
+						"type": "uint256"
+					},
+					{
+						"indexed": false,
+						"internalType": "address",
+						"name": "seller",
+						"type": "address"
+					}
+				],
+				"name": "AuctionResults",
+				"type": "event"
+			},
+			{
+				"anonymous": false,
+				"inputs": [
+					{
+						"indexed": false,
+						"internalType": "address",
+						"name": "bidder",
+						"type": "address"
+					},
+					{
+						"indexed": false,
+						"internalType": "uint256",
+						"name": "amount",
+						"type": "uint256"
+					}
+				],
+				"name": "BidWithdrawn",
+				"type": "event"
+			},
+			{
+				"anonymous": false,
+				"inputs": [
+					{
+						"indexed": false,
+						"internalType": "uint256",
+						"name": "gtin",
+						"type": "uint256"
+					},
+					{
+						"indexed": false,
+						"internalType": "string",
+						"name": "productName",
+						"type": "string"
+					},
+					{
+						"indexed": false,
+						"internalType": "uint256",
+						"name": "highestBid",
+						"type": "uint256"
+					}
+				],
+				"name": "NewBid",
+				"type": "event"
+			},
+			{
+				"inputs": [],
+				"name": "auctionCount",
+				"outputs": [
+					{
+						"internalType": "uint256",
+						"name": "",
+						"type": "uint256"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "auctionEnd",
+				"outputs": [],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "uint256",
+						"name": "",
+						"type": "uint256"
+					}
+				],
+				"name": "auctions",
+				"outputs": [
+					{
+						"internalType": "string",
+						"name": "productName",
+						"type": "string"
+					},
+					{
+						"internalType": "uint256",
+						"name": "gtin",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "price",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "auctionNum",
+						"type": "uint256"
+					},
+					{
+						"internalType": "enum ProductAuction.State",
+						"name": "state",
+						"type": "uint8"
+					},
+					{
+						"internalType": "uint256",
+						"name": "highestBid",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "auctionDuration",
+						"type": "uint256"
+					},
+					{
+						"internalType": "address payable",
+						"name": "seller",
+						"type": "address"
+					},
+					{
+						"internalType": "address payable",
+						"name": "buyer",
+						"type": "address"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "uint256",
+						"name": "_auctionNum",
+						"type": "uint256"
+					}
+				],
+				"name": "cancelAuction",
+				"outputs": [],
+				"stateMutability": "payable",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "string",
+						"name": "_productName",
+						"type": "string"
+					},
+					{
+						"internalType": "uint256",
+						"name": "_gtin",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "_price",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "_auctionDuration",
+						"type": "uint256"
+					}
+				],
+				"name": "createAuction",
+				"outputs": [
+					{
+						"internalType": "bool",
+						"name": "",
+						"type": "bool"
+					}
+				],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "uint256",
+						"name": "_auctionNum",
+						"type": "uint256"
+					}
+				],
+				"name": "makeBid",
+				"outputs": [],
+				"stateMutability": "payable",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "owner",
+				"outputs": [
+					{
+						"internalType": "address payable",
+						"name": "",
+						"type": "address"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "uint256",
+						"name": "_auctionNum",
+						"type": "uint256"
+					}
+				],
+				"name": "withdrawlBid",
+				"outputs": [],
+				"stateMutability": "payable",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "uint256",
+						"name": "_auctionNum",
+						"type": "uint256"
+					}
+				],
+				"name": "withdrawlProceeds",
+				"outputs": [],
+				"stateMutability": "payable",
+				"type": "function"
+			}
+		],
+		"devdoc": {
+			"author": "Alexander Poe",
+			"custom:experimental": "This is an experimental contract.",
+			"details": "All function calls are currently implemented without side effects",
+			"kind": "dev",
+			"methods": {},
+			"title": "An product auction for commodities ",
+			"version": 1
+		},
+		"userdoc": {
+			"kind": "user",
+			"methods": {
+				"constructor": {
+					"notice": "need function that works with time library to identify when current times passes the aunction duration, afer that it will set the state to ended"
+				},
+				"withdrawlBid(uint256)": {
+					"notice": "withdrawl bid after auction is over IF not winnner or if auction is cancelled "
+				}
+			},
+			"notice": "This contract is in a prototype ",
+			"version": 1
+		}
+	},
+	"settings": {
+		"compilationTarget": {
+			"contracts/finproj.sol": "ProductAuction"
+		},
+		"evmVersion": "london",
+		"libraries": {},
+		"metadata": {
+			"bytecodeHash": "ipfs"
+		},
+		"optimizer": {
+			"enabled": false,
+			"runs": 200
+		},
+		"remappings": []
+	},
+	"sources": {
+		"contracts/finproj.sol": {
+			"keccak256": "0x6cc264fcf8477cd832d2e1a266ef2a3bb75109745489a0b13fccb66e45812976",
+			"license": "GPL-3.0",
+			"urls": [
+				"bzz-raw://ee687abde5be46dcd1e9405bb20f0171ab5a6769ad08a9c8065c57055f0c67d4",
+				"dweb:/ipfs/QmeAQTgHB7MvYVPLvr7BCVDFUWXjGV4d12rRGszmf68a9e"
+			]
+		}
+	},
+	"version": 1
+}]
+
+
+// need a bidding function
+
+window.addEventListener('load', function() {
+  
+    if (typeof window.ethereum !== 'undefined') {
+      console.log('window.ethereum is enabled')
+      if (window.ethereum.isMetaMask === true) {
+        console.log('MetaMask is active')
+        let mmDetected = document.getElementById('mm-detected')
+        mmDetected.innerHTML += 'MetaMask Is Available!'
+  
+        // add in web3 here
+        var web3 = new Web3(window.ethereum)
+  
+      } else {
+        console.log('MetaMask is not available')
+        let mmDetected = document.getElementById('mm-detected')
+        mmDetected.innerHTML += 'MetaMask Not Available!'
+        // let node = document.createTextNode('<p>MetaMask Not Available!<p>')
+        // mmDetected.appendChild(node)
+      }
+    } else {
+      console.log('window.ethereum is not found')
+      let mmDetected = document.getElementById('mm-detected')
+      mmDetected.innerHTML += '<p>MetaMask Not Available!<p>'
+    }
+  })
+
+var web3 = new Web3(window.ethereum)
+
+
+
+const mmEnable = document.getElementById('mm-connect');
+
+// since MetaMask has been detected, we know
+// `ethereum` is an object, so we'll do the canonical
+// MM request to connect the account. 
+// 
+// typically we only request access to MetaMask when we
+// need the user to do something, but this is just for
+// an example
+ 
+mmEnable.onclick = async () => {
+  await ethereum.request({ method: 'eth_requestAccounts'})
+  // grab mm-current-account
+  // and populate it with the current address
+  var mmCurrentAccount = document.getElementById('mm-current-account');
+  mmCurrentAccount.innerHTML = 'Current Account: ' + ethereum.selectedAddress
+}
+
+const productAuction = new web3.eth.Contract(abi, address)
+productAuction.setProvider(window.ethereum)
+
+console.log(productAuction, "herer")
+
+const GetValue = document.getElementById('get-value')
+
+GetValue.onclick = async () => {
+
+    var web3 = new Web3(window.ethereum)
+  
+    const productAuction = new web3.eth.Contract(abi, address)
+    productAuction.setProvider(window.ethereum)
+  
+    var value = productAuction.methods.makeBid(1).call({from: ethereum.selectedAddress}).then(function(error, result){
+        console.log(error, result)
+    })
+  
+    const ssDisplayValue = document.getElementById('display-value')
+  
+    ssDisplayValue.innerHTML = 'Current Simple Storage Value: ' + value
+}
 // populate auctions
 populateAuctions = (array) => {
     auctions = []
@@ -129,4 +572,4 @@ document.getElementById("auctions").innerHTML +=
      <input id="price" type="text" placeholder="price">
      <input id="days" type="text" placeholder="days">
      <input type="button" onclick="submitAuction()" value="Submit">`
- }
+ };
